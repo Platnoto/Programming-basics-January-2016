4aafe8fcb8064dd51a056bfe02846f4158c67a5c
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 14:15:51 +0300

    Add files via upload

---
+++ 06-SquareOfStars.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_SquareOfStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', n));
+            for (int i = 1; i <= n - 2; i++)
+            {
+                string rowStar = '*' + new string  (' ',n - 2) + '*';
+                Console.WriteLine(rowStar);
+                    
+            }
+            Console.WriteLine(new string ('*',n));
+        }
+    }
+}
