9dac14b11600914ccb9800d9b124049a1d8a3e70
Radostin Gerdzhikov <rgerdzhikov@gmail.com>
2017-06-05 14:17:43 +0300

    Add files via upload

---
+++ 03-Nums 1...20.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Nums_1._._._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i =1; i <=20; i++) // i= +1
+
+            {
+                Console.WriteLine(i);
+                
+            }
+
+
+
+
+
+        }
+    }
+}
